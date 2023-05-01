4e128ccd65a1f1e9b67a6fad8c4e1fcc92ddb1e6
Dries Verachtert <dries.verachtert@epfl.ch>
2023-05-01 17:04:45 +0200

    BBPP154-40 Replace ssh key as AWS only supports certain types of ssh keys

--- ssh-keypairs.tf
+++ ssh-keypairs.tf
@@ -5,5 +5,5 @@
 # systems/services/external/aws/ssh/aws_coreservices_password
 resource "aws_key_pair" "aws_coreservices" {
   key_name   = "aws_coreservices"
-  public_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBqfeG3DiYkiO+RsDNADbhyXJJdYSXNnXQyVgSdu8l8W6U2h9kOXGlZeVdnSDMIlbkjvfabZwrH/cLQ3ITrbjnU= SSH key for AWS SBO POC"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO8QAh2WZ/WcZnNeojPNhadeodMO2l3PssaUFJWfvEFNzkuo5ci7nxb39M2FH6RyFAfqykV/v89KfDIg9K2ebJQZS+x6Enrqm7+ROmZjCdpYkFm7l2NCoKLus92DaPX6k1Tv5hcI76BqWN4nOKQxzb7ziJxFl5wzLgTwnXZvY33dA3Pu6aimksv071KnQ3hJKk6Omx/l7Hv/D7c0tU8vRCUefzHT3TkRpRgTTq+Wd8S0pGSmMB4drk5PiUzEVczxuIfmYGCWV2va6aT34yuMOw/6y2Cr9guCkyR2FkFm7q0MPw0aKGFBwTT05eiEWBWKQQbqi1qMtSwd6tp4qv6crN SSH key for AWS SBO POC"
 }
