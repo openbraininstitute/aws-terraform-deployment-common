b023351f63308a74a386d571fbb7f492b8dea8d7
acfleury <adrien.fleury@epfl.ch>
2023-06-06 14:42:18 +0200

    BBPP154-50 Load balancer for port 8200.

--- private-load-balancer.tf
+++ private-load-balancer.tf
@@ -51,6 +51,19 @@ resource "aws_vpc_security_group_ingress_rule" "alb_allow_9999_internal" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_8200_internal" {
+  security_group_id = aws_security_group.private_alb.id
+  description       = "Allow HTTPS from EPFL"
+  from_port         = 8200
+  to_port           = 8200
+  ip_protocol       = "tcp"
+  cidr_ipv4         = aws_vpc.sbo_poc.cidr_block
+
+  tags = {
+    Name = "private_alb_allow_8200_internal"
+  }
+}
+
 # TODO limit to only the listener ports and health check ports of the instance groups
 resource "aws_vpc_security_group_egress_rule" "alb_allow_everything_outgoing" {
   security_group_id = aws_security_group.private_alb.id
@@ -140,4 +153,37 @@ output "private_alb_listener_9999_id" {
 output "private_alb_listener_9999_arn" {
   description = "ARN of the listener on port 9999 for the private application load balancer"
   value       = aws_lb_listener.priv_alb_9999.arn
+}
+
+resource "aws_lb_listener" "priv_alb_8200" {
+  load_balancer_arn = aws_lb.private_alb.arn
+  port              = "8200"
+  #ts:skip=AC_AWS_0491
+  protocol = "HTTP" #tfsec:ignore:aws-elb-http-not-used
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content: port 8200 listener"
+      status_code  = "200"
+    }
+  }
+  tags = {
+    SBO_Billing = "common"
+  }
+  depends_on = [
+    aws_lb.private_alb
+  ]
+}
+
+output "private_alb_listener_8200_id" {
+  description = "ID of the listener on port 8200 for the private application load balancer"
+  value       = aws_lb_listener.priv_alb_8200.id
+}
+
+output "private_alb_listener_8200_arn" {
+  description = "ARN of the listener on port 8200 for the private application load balancer"
+  value       = aws_lb_listener.priv_alb_8200.arn
 }
