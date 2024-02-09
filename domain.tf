af8ca1a0e0c0e019748fe512fbc8944dc2004994
Pavlo Getta <pavlo.getta@epfl.ch>
2024-02-09 14:15:40 +0100

    [NOTICKET] Add production domains

--- domain.tf
+++ domain.tf
@@ -1,8 +1,28 @@
-# The domain that we use for the SBO POC: shapes-registry.org.
+# The domain that we use for the OBP POC: shapes-registry.org.
 # Not attached to a VPC because it has to be a public network.
 resource "aws_route53_zone" "domain" {
   name    = "shapes-registry.org"
-  comment = "Test domain for SBO POC"
+  comment = "Test domain for OBP POC"
+
+  tags = {
+    SBO_Billing = "common"
+  }
+}
+
+# Primary OBP domain openbrainplatform.org.
+resource "aws_route53_zone" "primary_domain" {
+  name    = "openbrainplatform.org"
+  comment = "Primary domain for OBP"
+
+  tags = {
+    SBO_Billing = "common"
+  }
+}
+
+# Secondary OBP domain openbrainplatform.com.
+resource "aws_route53_zone" "secondary_domain" {
+  name    = "openbrainplatform.com"
+  comment = "Secondary domain for OBP"
 
   tags = {
     SBO_Billing = "common"
--- outputs.tf
+++ outputs.tf
@@ -66,6 +66,26 @@ output "domain_arn" {
   value       = aws_route53_zone.domain.arn
 }
 
+output "primary_domain_zone_id" {
+  description = "zone_id of the domain openbrainplatform.org"
+  value       = aws_route53_zone.primary_domain.zone_id
+}
+
+output "primary_domain_arn" {
+  description = "ARN of the domain openbrainplatform.org"
+  value       = aws_route53_zone.primary_domain.arn
+}
+
+output "secondary_domain_zone_id" {
+  description = "zone_id of the domain openbrainplatform.com"
+  value       = aws_route53_zone.secondary_domain.zone_id
+}
+
+output "secondary_domain_arn" {
+  description = "ARN of the domain openbrainplatform.com"
+  value       = aws_route53_zone.secondary_domain.arn
+}
+
 output "vpc_arn" {
   description = "ARN of the VPC"
   value       = aws_vpc.sbo_poc.arn
