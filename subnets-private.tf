5eae01b463bf5053ea5372852ac0c7bcfece2869
Dries Verachtert <dries.verachtert@epfl.ch>
2024-02-16 16:28:35 +0100

    INFRA-9359 Move peering routing from compute subnet to all private subnets

--- subnets-private.tf
+++ subnets-private.tf
@@ -3,6 +3,10 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.sbo_poc.id
   route {
+    cidr_block                = aws_vpc.vpc_us_east_2.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.us_east_2.id
+  }
+  route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
