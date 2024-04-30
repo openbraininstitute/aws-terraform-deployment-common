990ef1132b25cc01a70f579a75e782d32e5fee53
Dries Verachtert <dries.verachtert@epfl.ch>
2024-04-30 15:26:22 +0200

    INFRA-9456 Migrate dockerhub secret from deployment-common to deployment repo

--- outputs.tf
+++ outputs.tf
@@ -117,17 +117,6 @@ output "bbpproxy_cidr" {
   value       = var.bbpproxy_cidr
 }
 
-output "dockerhub_credentials_arn" {
-  description = "ARN of the secret containing the dockerhub credentials"
-  value       = var.dockerhub_credentials_arn
-  sensitive   = true
-}
-
-output "dockerhub_access_iam_policy_arn" {
-  description = "ARN of the IAM policy that gives access to the dockerhub credential"
-  value       = aws_iam_policy.dockerhub_access.arn
-}
-
 output "route_table_private_subnets_arn" {
   description = "ARN of the route table for all private subnets in the main region"
   value       = module.network.private_route_table_arn
