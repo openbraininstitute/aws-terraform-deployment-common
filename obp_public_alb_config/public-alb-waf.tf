2a53b5da88e16c8f122fe7b71a1598e7a97a5f03
Erik Heeren <erik.heeren@epfl.ch>
2024-09-02 10:22:15 +0000

    BBPP154-297: Enable core ruleset: fix typo

--- obp_public_alb_config/public-alb-waf.tf
+++ obp_public_alb_config/public-alb-waf.tf
@@ -39,7 +39,7 @@ resource "aws_wafv2_web_acl" "basic_protection" {
             count {}
           }
 
-          name = "SizeRestrictions_Body"
+          name = "SizeRestrictions_BODY"
         }
       }
     }
