1e1082bf7c9e7ae85fdc0592159ac88973ec8484
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-03-17 12:23:54 +0100

    Retain aws-waf-logs for 30 days instead of 7

--- obp_private_alb_config/private-alb-waf.tf
+++ obp_private_alb_config/private-alb-waf.tf
@@ -276,7 +276,7 @@ resource "aws_wafv2_web_acl_association" "waf_association" {
 #tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "waf_logs" {
   name              = "aws-waf-logs-private-loadbalancer"
-  retention_in_days = 7
+  retention_in_days = 30
   tags_all = {
     Name = "aws-waf-logs-private-loadbalancer"
   }
