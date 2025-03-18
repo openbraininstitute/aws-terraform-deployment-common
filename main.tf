68872ecdd6580ae01959128cbdcd6cd8ca991b32
Dries Verachtert <dries.verachtert@dries.eu>
2025-03-18 13:18:37 +0100

    Validate the next.staging.openbraininstitute.org with the validation domain openbraininstitute.org

--- main.tf
+++ main.tf
@@ -191,6 +191,8 @@ module "next_staging_openbraininstitute_org_cert" {
   source = "./tls_certificate_without_domain"
 
   hostname = "next.staging.openbraininstitute.org"
+
+  validation_domain = "openbraininstitute.org"
 }
 
 module "openbraininstitute_org_cert" {
--- tls_certificate_without_domain/variables.tf
+++ tls_certificate_without_domain/variables.tf
@@ -3,3 +3,10 @@ variable "hostname" {
   sensitive   = false
   description = "The hostname for which you need to an SSL / TLS certificate"
 }
+
+variable "validation_domain" {
+  type        = string
+  sensitive   = false
+  default     = null
+  description = "superdomain which can be used to receive validation emails, should have a hostmaster@ type of email alias"
+}
