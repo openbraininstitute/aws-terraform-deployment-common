452faf060718cd240035dd6735ba4cebf7b7820d
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-03-31 16:39:41 +0200

    Disable exceptions now that internal traffic is allowed

--- obp_private_alb_config/private-alb-waf.tf
+++ obp_private_alb_config/private-alb-waf.tf
@@ -88,40 +88,6 @@ resource "aws_wafv2_web_acl" "basic_protection" {
 
           name = "SizeRestrictions_BODY"
         }
-        rule_action_override {
-          # Ignore SSRF Query Arguments - we'll deal with those in a later rule
-          action_to_use {
-            count {}
-          }
-
-          name = "EC2MetaDataSSRF_QUERYARGUMENTS"
-        }
-        rule_action_override {
-          # This messes with Keycloak, it's unclear whether it's for dev setups only.
-          # Once openbluebrain is publicly accessible, evaluate whether we need to add an exception
-          action_to_use {
-            count {}
-          }
-
-          name = "EC2MetaDataSSRF_BODY"
-        }
-        rule_action_override {
-          # This messes with Keycloak, it's unclear whether it's for dev setups only.
-          # Once openbluebrain is publicly accessible, evaluate whether we need to add an exception
-          action_to_use {
-            count {}
-          }
-
-          name = "GenericRFI_BODY"
-        }
-        rule_action_override {
-          # Some file uploads run into this
-          action_to_use {
-            count {}
-          }
-
-          name = "CrossSiteScripting_BODY"
-        }
       }
     }
 
