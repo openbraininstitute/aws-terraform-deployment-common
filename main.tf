d41ce0c5d162b9df1e6d5e7b1eff612bef361b0c
Daniel Fernández <daferoes@gmail.com>
2025-08-18 09:54:38 +0200

    Add certificate for jupyterhub_openbraininstitute_org

--- main.tf
+++ main.tf
@@ -53,6 +53,7 @@ module "private_alb_basic" {
     module.openbluebrain_com_cert.certificate_arn,
     module.www_openbluebrain_com_cert.certificate_arn,
     module.jupyterhub_openbrainplatform_com_cert.certificate_arn,
+    module.jupyterhub_openbraininstitute_org_cert.certificate_arn,
     module.www_openbrainplatform_org_cert.certificate_arn,
     module.openbrainplatform_com_cert.certificate_arn,
     module.www_openbrainplatform_com_cert.certificate_arn,
@@ -153,6 +154,13 @@ module "alt_private_domain_openbrainplatform_org" {
   private_alb_zone_id  = module.private_alb_basic.alb_zone_id
   comment              = "Alternative domain openbrainplatform.org"
   vpc_id               = module.network.vpc_id
+}
+
+module "jupyterhub_openbraininstitute_org_cert" {
+  source = "./tls_certificate"
+
+  hostname = module.jupyterhub_openbraininstitute_org.domain_name
+  zone_id  = module.jupyterhub_openbraininstitute_org.domain_zone_id
 }
 
 module "openbrainplatform_org_cert" {
