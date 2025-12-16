1d432541d094ba3dcce037bb66befc3cd0254249
Dries Verachtert <dries.verachtert@dries.eu>
2025-12-16 09:44:09 +0100

    In AWS, you can only have a single VPN Gateway, but it can connect to multiple Customer Gateways
    https://github.com/openbraininstitute/INFRA/issues/364

--- main.tf
+++ main.tf
@@ -326,15 +326,21 @@ module "vpn_to_azure" {
 
   count = var.is_staging ? 1 : 0
 
-  vpc_id        = module.network.vpc_id
-  aws_bgp_asn   = 65000
-  azure_bgp_asn = 65515
+  vpc_id         = module.network.vpc_id
+  aws_bgp_asn    = 65000
+  azure_bgp_asn1 = 65515
+  azure_bgp_asn2 = 65516
+
   # azure_vpn_gateway_tunnel1_ip_address = var.azure_vpn_gateway_tunnel1_ip_address
-  tunnel1_preshared_key                 = var.azure_vpn_gateway_tunnel1_preshared_key1
-  tunnel2_preshared_key                 = var.azure_vpn_gateway_tunnel1_preshared_key2
-  aws_region                            = var.aws_region
-  azure_vpn_gateway_tunnel1_inside_cidr = "169.254.21.0/30"
-  azure_vpn_gateway_tunnel2_inside_cidr = "169.254.21.4/30"
+  tunnel1_preshared_key1                 = var.azure_vpn_gateway_tunnel1_preshared_key1
+  tunnel1_preshared_key2                 = var.azure_vpn_gateway_tunnel1_preshared_key2
+  tunnel2_preshared_key1                 = var.azure_vpn_gateway_tunnel2_preshared_key1
+  tunnel2_preshared_key2                 = var.azure_vpn_gateway_tunnel2_preshared_key2
+  aws_region                             = var.aws_region
+  azure_vpn_gateway_tunnel1_inside_cidr1 = "169.254.21.0/30"
+  azure_vpn_gateway_tunnel1_inside_cidr2 = "169.254.21.4/30"
+  azure_vpn_gateway_tunnel2_inside_cidr1 = "169.254.21.8/30"
+  azure_vpn_gateway_tunnel2_inside_cidr2 = "169.254.21.12/30"
 
   # define the domain which should be used to create a cname record
   # full names become in staging vpn-for-azure1.staging.openbrainplatform.org
@@ -344,35 +350,10 @@ module "vpn_to_azure" {
   zone_id    = module.alt_domain_openbrainplatform_org.domain_zone_id
 
   azure_vpn_gateway_tunnel1_ip_address = var.azure_vpn_gateway_tunnel1_ip_address
+  azure_vpn_gateway_tunnel2_ip_address = var.azure_vpn_gateway_tunnel2_ip_address
 
-  providers = {
-    aws = aws.site_to_site_vpn
-  }
-}
-
-module "vpn_to_azure_south_central_us" {
-  source = "./site_to_site_vpn"
-
-  count = var.is_staging ? 1 : 0
-
-  vpc_id        = module.network.vpc_id
-  aws_bgp_asn   = 65000
-  azure_bgp_asn = 65515
-  # azure_vpn_gateway_tunnel1_ip_address = var.azure_vpn_gateway_tunnel1_ip_address
-  tunnel1_preshared_key                 = var.azure_vpn_gateway_tunnel2_preshared_key1
-  tunnel2_preshared_key                 = var.azure_vpn_gateway_tunnel2_preshared_key2
-  aws_region                            = var.aws_region
-  azure_vpn_gateway_tunnel1_inside_cidr = "169.254.21.0/30"
-  azure_vpn_gateway_tunnel2_inside_cidr = "169.254.21.4/30"
-
-  # define the domain which should be used to create a cname record
-  # full names become in staging vpn-for-azure1.staging.openbrainplatform.org
-  # and vpn-for-azure2.staging.openbrainplatform.org
-  cname_base = "vpn-for-azure-south-central-us"
-  domainname = module.alt_domain_openbrainplatform_org.domain_name
-  zone_id    = module.alt_domain_openbrainplatform_org.domain_zone_id
-
-  azure_vpn_gateway_tunnel1_ip_address = var.azure_vpn_gateway_tunnel2_ip_address
+  vpn1_to_azure_enabled = true
+  vpn2_to_azure_enabled = true
 
   providers = {
     aws = aws.site_to_site_vpn
--- site_to_site_vpn/variables.tf
+++ site_to_site_vpn/variables.tf
@@ -10,8 +10,14 @@ variable "aws_bgp_asn" {
   sensitive   = false
 }
 
-variable "azure_bgp_asn" {
-  description = "The BGP ASN number on the Azure side"
+variable "azure_bgp_asn1" {
+  description = "The BGP ASN number on the Azure side 2"
+  type        = number
+  sensitive   = false
+}
+
+variable "azure_bgp_asn2" {
+  description = "The BGP ASN number on the Azure side 2"
   type        = number
   sensitive   = false
 }
@@ -22,14 +28,32 @@ variable "azure_vpn_gateway_tunnel1_ip_address" {
   sensitive   = false
 }
 
-variable "tunnel1_preshared_key" {
-  description = "The first preshared key for the Azure VPN Gateway"
+variable "azure_vpn_gateway_tunnel2_ip_address" {
+  description = "The IP address of the tunnel interface on the Azure VPN Gateway"
+  type        = string
+  sensitive   = false
+}
+
+variable "tunnel1_preshared_key1" {
+  description = "The first preshared key for the Azure VPN Gateway tunnel 1"
   type        = string
   sensitive   = true
 }
 
-variable "tunnel2_preshared_key" {
-  description = "The second preshared key for the Azure VPN Gateway"
+variable "tunnel1_preshared_key2" {
+  description = "The second preshared key for the Azure VPN Gateway tunnel 1"
+  type        = string
+  sensitive   = true
+}
+
+variable "tunnel2_preshared_key1" {
+  description = "The first preshared key for the Azure VPN Gateway tunnel 2"
+  type        = string
+  sensitive   = true
+}
+
+variable "tunnel2_preshared_key2" {
+  description = "The second preshared key for the Azure VPN Gateway tunnel 2"
   type        = string
   sensitive   = true
 }
@@ -40,14 +64,26 @@ variable "aws_region" {
   sensitive   = false
 }
 
-variable "azure_vpn_gateway_tunnel1_inside_cidr" {
-  description = "The inside CIDR, within tunnel 1, has to be a /30 in RFC 6890"
+variable "azure_vpn_gateway_tunnel1_inside_cidr1" {
+  description = "The inside CIDR 1, within tunnel 1, has to be a /30 in RFC 6890"
   type        = string
   sensitive   = false
 }
 
-variable "azure_vpn_gateway_tunnel2_inside_cidr" {
-  description = "The inside CIDR, within tunnel 2, has to be a /30 in RFC 6890"
+variable "azure_vpn_gateway_tunnel1_inside_cidr2" {
+  description = "The inside CIDR2 , within tunnel 1, has to be a /30 in RFC 6890"
+  type        = string
+  sensitive   = false
+}
+
+variable "azure_vpn_gateway_tunnel2_inside_cidr1" {
+  description = "The inside CIDR 1, within tunnel 2, has to be a /30 in RFC 6890"
+  type        = string
+  sensitive   = false
+}
+
+variable "azure_vpn_gateway_tunnel2_inside_cidr2" {
+  description = "The inside CIDR2 , within tunnel 2, has to be a /30 in RFC 6890"
   type        = string
   sensitive   = false
 }
@@ -68,4 +104,16 @@ variable "cname_base" {
   type        = string
   sensitive   = false
   description = "The first part of the cname that points to the VPN public ip address, e.g. vpn becomes vpn1 and vpn2"
+}
+
+variable "vpn1_to_azure_enabled" {
+  type        = bool
+  sensitive   = false
+  description = "Enable or disable vpn1 to azure"
+}
+
+variable "vpn2_to_azure_enabled" {
+  type        = bool
+  sensitive   = false
+  description = "Enable or disable vpn1 to azure"
 }
