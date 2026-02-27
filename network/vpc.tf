b38a195a651e7e4b7ddf10306053dfa0e9a22adf
Daniel Fernández <daferoes@gmail.com>
2026-02-27 10:04:29 +0100

    Delete duplicated default security group resource
    VPC 'default' security group is managed in the deployment repo:
    https://github.com/openbraininstitute/aws-terraform-deployment/commit/abc93b4b9538a17c3bc539aa2db3cc71423d042f

--- network/vpc.tf
+++ network/vpc.tf
@@ -11,19 +11,3 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Default security group under Terraform management (e.g., for tagging)
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.main_vpc.id
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
