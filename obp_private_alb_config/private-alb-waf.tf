d9b2b29b74ac187f9f4665b7c51e7147d417a492
Erik Heeren <erik.heeren@openbraininstitute.org>
2026-05-12 11:49:02 +0200

    Allow insecure transport to WAF S3 logging bucket

--- obp_private_alb_config/private-alb-waf.tf
+++ obp_private_alb_config/private-alb-waf.tf
@@ -423,7 +423,7 @@ resource "aws_s3_bucket_policy" "aws_waf_logs_bucket" {
     Statement = [
       {
         Sid       = "DenyInsecureTransport"
-        Effect    = "Deny"
+        Effect    = "Allow" # to verify that this is the problem
         Principal = "*"
         Action    = "s3:*"
         Resource = [
